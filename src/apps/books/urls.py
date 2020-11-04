121b5e231de578db3a1f5c64cd5b85fd0f40ba1b
Alexey Kotenko <k0t3n.mail@gmail.com>
2020-11-04 11:20:21 +0300

    Add Travis CI (#1)

--- src/apps/books/urls.py
+++ src/apps/books/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import SimpleRouter
 from .views import BookViewSet
 
 router = SimpleRouter()
-router.register('', BookViewSet, basename='')
+router.register('', BookViewSet)
 
 urlpatterns = router.urls
